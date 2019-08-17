f0c7035243541499f5e633618b3c9aaec3cdfb0c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-17 16:14:10 +0300

    simple repository

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Topic;
 use Illuminate\Http\Request;
+use App\Repositories\Contracts\TopicRepository;
 
 class TopicController extends Controller
 {
+    protected $topics;
+
+    public function __construct(TopicRepository $topics)
+    {
+        $this->topics = $topics;
+    }
+
     public function index()
     {
-        $topics = Topic::get();
-
-        dd($topics);
+        return $this->topics->all();
     }
 }
