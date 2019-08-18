e3349df47561b933dae8dae9436b1ae6c23f0e99
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-18 19:34:20 +0300

    creating through relationships

--- app/Repositories/Eloquent/EloquentUserRepository.php
+++ app/Repositories/Eloquent/EloquentUserRepository.php
@@ -8,6 +8,16 @@ use App\Repositories\RepositoryAbstract;
 
 class EloquentUserRepository extends RepositoryAbstract implements UserRepository
 {
+    public function createAddress($userId, array $properties)
+    {
+        return $this->find($userId)->addresses()->create($properties);
+    }
+
+    public function deleteAddress($userId, $addressId)
+    {
+        return $this->find($userId)->addresses()->findOrFail($addressId)->delete();
+    }
+
     public function entity()
     {
         return User::class;
